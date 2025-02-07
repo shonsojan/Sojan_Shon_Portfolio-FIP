9b41fcc0f9720e299bb2818e20e800f87884b145
shonsojan <shonsojan2004@gmail.com>
2025-02-07 17:02:44 -0500

    deleted

--- includes/connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-//Creates a connection to the database. This code is 'included' into another file, as if it is pasted into the other file.
-$connect = new mysqli('localhost','root','','portfolio');
-?>
